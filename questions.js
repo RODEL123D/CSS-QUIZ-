97f1ed8cc8ac9dffc8b57722b5411ccc769ad767
RODEL123D <102315661+RODEL123D@users.noreply.github.com>
2022-04-02 14:03:51 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+var questions =[
+    {
+num : 1,
+question : "What does CSS stand for?",
+answer : "Cascading Style Sheets",
+options : [
+"Cascading Style Sheets",
+"Colorful Style Sheets",
+"Computer Style Sheets",
+"Creative Style Sheets"
+        ]
+    },
+    {
+num: 2,
+question: "How do you insert a comment in a CSS file?",
+answer: "/* this is a comment */",
+options: [
+"/* this is a comment */",
+"// this is a comment //",
+"' this is a comment",
+"// this is a comment"
+]
+},
+{
+num: 3,
+question: "Which CSS property controls the text size?",
+answer: "font-size",
+options: [
+"text-size",
+"font-size",
+"text-style",
+"font-style"
+]
+},
+
+{
+num: 4,
+question: "How do you make each word in a text start with a capital letter?",
+answer: "text-transform:capitalize",
+options: [
+"text-transform:capitalize",
+"text-style:capitalize",
+"You can't do that with CSS",
+"transform:capitalize"
+]
+},
+{
+num: 5,
+question: "How do you group selectors?",
+answer: "Separate each selector with a comma",
+options: [
+"Separate each selector with a space",
+"Separate each selector with a comma",
+"Separate each selector with a plus sign",
+"All of the above"
+]
+},
+
+
+
+
+];
